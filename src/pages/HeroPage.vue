14a34d8b5d4f09417af6946ddb23ce306c8e2a20
mkosinski <m.kosinskki@gmail.com>
2025-11-24 20:49:35 +0100

    Changed styles, added on hover effects.

--- src/pages/HeroPage.vue
+++ src/pages/HeroPage.vue
@@ -35,7 +35,7 @@
                     <img src="../components/papuzka.png" alt="parrot" class="w-60 h-auto md:w-80">
                 </div>
             </div>
-            <svg class="absolute bottom-0 left-0 w-full h-auto" width="1920" height="230" viewBox="0 0 1920 230"
+            <svg class="absolute -bottom-0.5 left-0 w-full h-auto" width="1920" height="230" viewBox="0 0 1920 230"
                 fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path
                     d="M0 69.7185L80 48.4185C160 26.7185 320 -15.2815 480 5.71853C640 26.7185 800 112.719 960 133.719C1120 154.719 1280 112.719 1440 91.0185C1600 69.7185 1760 69.7185 1840 69.7185H1920V229.719H1840C1760 229.719 1600 229.719 1440 229.719C1280 229.719 1120 229.719 960 229.719C800 229.719 640 229.719 480 229.719C320 229.719 160 229.719 80 229.719H0V69.7185Z"
