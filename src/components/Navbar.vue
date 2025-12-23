0af2f77be1468905cf31aa33d7785c2c0a9e6043
mkosinski <m.kosinskki@gmail.com>
2025-12-23 17:21:12 +0100

    Some small changes, added zara wedding to projects

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,129 +1,142 @@
 <template>
-    <nav class="w-full sticky top-0 bg-offWhite shadow-md z-50">
-        <div class="container max-w-6xl mx-auto px-5 py-5 flex items-center justify-between">
+  <nav class="w-full sticky top-0 bg-offWhite shadow-md z-50">
+    <div class="container max-w-6xl mx-auto px-5 py-5 flex items-center justify-between">
+      <div class="z-50">
+        <a href="#hero" class="text-2xl font-bold text-mediumGreen"> Marcin Kosiński </a>
+      </div>
 
-            <div class="z-50">
-                <a href="#hero" class="text-2xl font-bold text-mediumGreen">
-                    Marcin Kosiński
-                </a>
-            </div>
+      <button
+        id="menu-btn"
+        type="button"
+        class="z-50 block hamburger md:hidden focus:outline-none"
+        :class="{ open: isMenuOpen }"
+        @click="toggleMenu"
+      >
+        <span class="hamburger-top"></span>
+        <span class="hamburger-middle"></span>
+        <span class="hamburger-bottom"></span>
+      </button>
 
-            <button id="menu-btn" type="button" class="z-50 block hamburger md:hidden focus:outline-none"
-                :class="{ open: isMenuOpen }" @click="toggleMenu">
-                <span class="hamburger-top"></span>
-                <span class="hamburger-middle"></span>
-                <span class="hamburger-bottom"></span>
-            </button>
+      <div class="hidden md:flex items-center gap-4 lg:gap-12">
+        <a
+          v-for="item in menuItems"
+          :key="item.name"
+          :href="item.path"
+          class="group relative text-graphite hover:text-mediumGreen hover:bg-lightMint hover:rounded-md font-medium px-1 py-1 flex justify-center items-center transition-colors duration-300"
+        >
+          <span class="relative z-10 bg-offWhite px-2.5 rounded-sm text-lg">{{ item.name }}</span>
+          <LeafPair side="right" />
+          <LeafPair side="left" />
+        </a>
+      </div>
+    </div>
 
-            <div class="hidden md:flex items-center gap-4 lg:gap-12">
-                <component v-for="item in menuItems" :key="item.name" :is="item.path.startsWith('#') ? 'a' : RouterLink"
-                    :href="item.path.startsWith('#') ? item.path : undefined"
-                    :to="item.path.startsWith('#') ? undefined : item.path"
-                    class="group relative text-graphite hover:text-mediumGreen hover:bg-lightMint hover:rounded-md font-medium px-1 py-1 flex justify-center items-center transition-colors duration-300">
-                    <span class="relative z-10 bg-offWhite px-2.5 rounded-sm text-lg">{{ item.name }}</span>
-                    <LeafPair side="right" />
-                    <LeafPair side="left" />
-                </component>
-            </div>
-        </div>
+    <Transition name="fade">
+      <div
+        v-if="isMenuOpen"
+        class="absolute top-full left-0 w-full bg-offWhite border-t border-gray-300 shadow-lg flex flex-col items-center py-8 space-y-6 font-bold md:hidden z-40"
+      >
+        <a
+          v-for="item in menuItems"
+          :key="item.name"
+          :href="item.path"
+          @click="toggleMenu"
+          class="group relative text-graphite font-medium px-4 py-2 flex justify-center items-center text-xl w-full hover:bg-lightMint/20"
+        >
+          <span class="relative z-10 px-2.5">{{ item.name }}</span>
+          <LeafPair side="right" />
+          <LeafPair side="left" />
+        </a>
+      </div>
+    </Transition>
 
-        <Transition name="fade">
-            <div v-if="isMenuOpen"
-                class="absolute top-full left-0 w-full bg-offWhite border-t border-gray-300 shadow-lg flex flex-col items-center py-8 space-y-6 font-bold md:hidden z-40">
-                <component v-for="item in menuItems" :key="item.name" :is="item.path.startsWith('#') ? 'a' : RouterLink"
-                    :href="item.path.startsWith('#') ? item.path : undefined"
-                    :to="item.path.startsWith('#') ? undefined : item.path" @click="toggleMenu"
-                    class="group relative text-graphite font-medium px-4 py-2 flex justify-center items-center text-xl w-full hover:bg-lightMint/20">
-                    <span class="relative z-10 px-2.5">{{ item.name }}</span>
-                    <LeafPair side="right" />
-                    <LeafPair side="left" />
-                </component>
-            </div>
-        </Transition>
-
-        <Transition name="fade">
-            <div v-if="isMenuOpen" @click="toggleMenu"
-                class="fixed inset-x-0 bottom-0 top-20 -z-10 bg-black/20 backdrop-blur-sm md:hidden h-screen">
-            </div>
-        </Transition>
-    </nav>
+    <Transition name="fade">
+      <div
+        v-if="isMenuOpen"
+        @click="toggleMenu"
+        class="fixed inset-x-0 bottom-0 top-20 -z-10 bg-black/20 backdrop-blur-sm md:hidden h-screen"
+      ></div>
+    </Transition>
+  </nav>
 </template>
 
 <script setup>
-import { ref } from 'vue';
-import { RouterLink } from 'vue-router';
-import LeafPair from './LeafPair.vue';
+import { ref } from 'vue'
+import { RouterLink } from 'vue-router'
+import LeafPair from './LeafPair.vue'
 
-const isMenuOpen = ref(false);
+const isMenuOpen = ref(false)
 
 const toggleMenu = () => {
-    isMenuOpen.value = !isMenuOpen.value;
-};
+  isMenuOpen.value = !isMenuOpen.value
+}
 
 const menuItems = [
-    { name: 'O mnie', path: '#about-me' },
-    { name: 'Tech Stack', path: '#technologies' },
-    { name: 'Projekty', path: '#projects' },
-    { name: 'Edukacja', path: '#education' },
-    { name: 'Kontakt', path: '#contact' },
-];
+  { name: 'O mnie', path: '#about-me' },
+  { name: 'Tech Stack', path: '#technologies' },
+  { name: 'Projekty', path: '#projects' },
+  { name: 'Edukacja', path: '#education' },
+  { name: 'Kontakt', path: '#contact' },
+]
 </script>
 
 <style scoped>
 .hamburger {
-    cursor: pointer;
-    width: 24px;
-    height: 24px;
-    transition: all 0.25s;
-    position: relative;
+  cursor: pointer;
+  width: 24px;
+  height: 24px;
+  transition: all 0.25s;
+  position: relative;
 }
 
 .hamburger-top,
 .hamburger-middle,
 .hamburger-bottom {
-    position: absolute;
-    width: 24px;
-    height: 2px;
-    top: 0;
-    left: 0;
-    background: #2c2c2c;
-    transform: rotate(0);
-    transition: all 0.5s;
+  position: absolute;
+  width: 24px;
+  height: 2px;
+  top: 0;
+  left: 0;
+  background: #2c2c2c;
+  transform: rotate(0);
+  transition: all 0.5s;
 }
 
 .hamburger-middle {
-    transform: translateY(7px);
+  transform: translateY(7px);
 }
 
 .hamburger-bottom {
-    transform: translateY(14px);
+  transform: translateY(14px);
 }
 
 .open {
-    transform: rotate(90deg);
-    transform-origin: center;
+  transform: rotate(90deg);
+  transform-origin: center;
 }
 
 .open .hamburger-top {
-    transform: rotate(45deg) translateY(6px) translateX(6px);
+  transform: rotate(45deg) translateY(6px) translateX(6px);
 }
 
 .open .hamburger-middle {
-    display: none;
+  display: none;
 }
 
 .open .hamburger-bottom {
-    transform: rotate(-45deg) translateY(6px) translateX(-6px);
+  transform: rotate(-45deg) translateY(6px) translateX(-6px);
 }
 
 .fade-enter-active,
 .fade-leave-active {
-    transition: opacity 0.3s ease, transform 0.3s ease;
+  transition:
+    opacity 0.3s ease,
+    transform 0.3s ease;
 }
 
 .fade-enter-from,
 .fade-leave-to {
-    opacity: 0;
-    transform: translateY(-10px);
+  opacity: 0;
+  transform: translateY(-10px);
 }
 </style>
--- src/pages/AboutMe.vue
+++ src/pages/AboutMe.vue
@@ -1,60 +1,65 @@
 <template>
-    <section id="about-me" class="bg-cotton relative">
+  <section id="about-me" class="bg-cotton relative">
+    <div
+      class="container mx-auto flex flex-col-reverse md:flex-row text-offWhite justify-center items-center min-h-[calc(100vh-72px)] px-10 py-10 space-y-10 md:px-12 md:space-y-0 md:space-x-10"
+    >
+      <div class="w-full md:w-1/2 flex flex-col justify-center items-center">
+        <div class="group mt-20 md:mt-0">
+          <img
+            src="../components/profile.jpg"
+            alt="zdjecie marcina"
+            class="w-60 h-auto md:w-96 group-hover:scale-105 duration-150 rounded-t-lg"
+          />
 
-        <div
-            class="container mx-auto flex flex-col-reverse md:flex-row text-offWhite justify-center items-center min-h-[calc(100vh-72px)] px-10 py-10 space-y-10 md:px-12 md:space-y-0 md:space-x-10">
-            <div class="w-full md:w-1/2 flex flex-col justify-center items-center">
-                <div class="group mt-20 md:mt-0">
-                    <img src="https://placehold.co/700x900/" alt="zdjecie marcina"
-                        class="w-60 h-auto md:w-96 group-hover:scale-105 duration-150 rounded-t-lg">
-
-                    <a href="#contact" class="block group-hover:scale-105 transition-transform duration-150">
-                        <div
-                            class="bg-lightMint text-graphite py-4 rounded-b-lg text-center font-semibold transition-colors duration-150 group-hover:bg-forestGreen group-hover:text-offWhite group-hover:shadow-lg">
-                            Skontaktuj się ze mną!
-                        </div>
-                    </a>
-                </div>
-            </div>
-
+          <a href="#contact" class="block group-hover:scale-105 transition-transform duration-150">
             <div
-                class="w-full md:w-1/2 items-center justify-center space-y-8 leading-9 text-lg md:text-xl font-semibold text-graphite mt-10 md:mt-0">
-                <div class="flex flex-col space-y-8 text-center md:max-w-xl md:text-left">
-                    <h2 class="text-4xl md:text-5xl font-extrabold text-center md:text-left text-mediumGreen">
-                        Kod, który robi różnicę
-                    </h2>
-
-                    <p class="text-mediumGreen text-2xl md:text-3xl font-bold text-center md:text-left my-8">Cześć!</p>
-
-                    <p>Jestem studentem informatyki, aktualnie jestem na III roku studiów
-                        na
-                        Politechnice Białostockiej. </p>
-
-                    <p>Głęboko wierzę, że <span class="text-mediumGreen">najlepszą nauką
-                            jest
-                            praktyka.</span> Teoria jest
-                        istotna,
-                        wręcz niezbędna, ale moim
-                        największym celem jest przekuwanie jej w widoczne rezultaty.</p>
-
-                    <p>Fascynuje mnie proces tworzenia oprogramowania - od analizy
-                        problemu,
-                        przez projekt aż po realnie
-                        działający produkt. Aktualnie skupiam się na poszerzaniu swojej wiedzy w zakresie pełnego
-                        tworzenia
-                        stron <span class="text-mediumGreen">(fullstack developer)</span>. </p>
-
-                    <p><span class="text-mediumGreen">W wolnym czasie</span>, uwielbiam
-                        uprawiać
-                        różnego rodzaju sporty: treningi
-                        siłowe, squash, jazda na rowerze i
-                        wiele więcej. </p>
-                </div>
-
+              class="bg-lightMint text-graphite py-4 rounded-b-lg text-center font-semibold transition-colors duration-150 group-hover:bg-forestGreen group-hover:text-offWhite group-hover:shadow-lg"
+            >
+              Skontaktuj się ze mną!
             </div>
+          </a>
         </div>
-    </section>
+      </div>
 
+      <div
+        class="w-full md:w-1/2 items-center justify-center space-y-8 leading-9 text-lg md:text-xl font-semibold text-graphite mt-10 md:mt-0"
+      >
+        <div class="flex flex-col space-y-8 text-center md:max-w-xl md:text-justify">
+          <h2 class="text-4xl md:text-5xl font-extrabold text-center md:text-left text-mediumGreen">
+            Kod, który robi różnicę
+          </h2>
+
+          <p class="text-mediumGreen text-2xl md:text-3xl font-bold text-center md:text-left my-8">
+            Cześć!
+          </p>
+
+          <p>
+            Jestem studentem informatyki, aktualnie jestem na III roku studiów na Politechnice
+            Białostockiej.
+          </p>
+
+          <p>
+            Głęboko wierzę, że
+            <span class="text-mediumGreen">najlepszą nauką jest praktyka.</span> Teoria jest
+            istotna, wręcz niezbędna, ale moim największym celem jest przekuwanie jej w widoczne
+            rezultaty.
+          </p>
+
+          <p>
+            Fascynuje mnie proces tworzenia oprogramowania - od analizy problemu, przez projekt aż
+            po realnie działający produkt. Aktualnie bardziej
+            <span class="text-mediumGreen"> Frontend Developer</span>, ale docelowo chciałbym
+            rozwijać się jako <span class="text-mediumGreen"> Fullstack Developer. </span>
+          </p>
+
+          <p>
+            <span class="text-mediumGreen">W wolnym czasie</span>, uwielbiam uprawiać różnego
+            rodzaju sporty: treningi siłowe, squash, jazda na rowerze i wiele więcej.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script setup></script>
